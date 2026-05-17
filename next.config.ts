8fd589e6183babbc07d1bf2034749b4beea6f4d6
Vernon Venter <vee1ster@gmail.com>
2026-05-17 09:18:36 +0200

    login page redirect

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Move this to the top level, NO "experimental" or "turbo" wrapper
   serverExternalPackages: ["@prisma/client", "pg"],
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false, // Set to false for development so it doesn't cache permanently
+      },
+    ];
+  }
 };
 
 export default nextConfig;
+
